091eb98cb4db0aa0da189fd3632cd815e98a3cde
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-12 07:54:15 +0700

    Update Diaogue Editor
    add method OnOpenAset Dialogue
    method ini berfungsi memanggil window dialogeditor saat kita menekan aset dialog2x, sama seperti aset lainnya,
    
    [OnOpenAssetAttribute(1)] ini menandakan bagwa metode ini akan di eksekusi ketika sebuah aset dibuka di unityeditor
    
    jadi setiap object/aset di unity/editor memiliki ID unik masing" , nah disinilah kita membuat casting supaya hanya aset dialog yang bisa membuka window dialog editor
    
    EditorUtility.InstanceIDToObject ini adalah metode statis dari unity editor Utility untuk mendapatkan object dengan id aset yg diberikan lalu hasilnya di casting ke dialogue,
    
    setelah itu tinggal buat kondisi jika dialognya tidak null , kita bisa membuka window dialogue ketika menekan aset dialoge tsb di editor

--- Assets/Scripts/Editor/DialogueEditor.cs
+++ Assets/Scripts/Editor/DialogueEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 namespace DialogueSystem.Editor
@@ -10,8 +11,22 @@ namespace DialogueSystem.Editor
         [MenuItem("Window/DialogueEditor")]
         public static void ShowEditorWindow()
         {
-            GetWindow(typeof(DialogueEditor),false,"Asura Dialogue Editor");
+            Debug.Log("anda mengaktifkan dialogueEditor");
+            GetWindow(typeof(DialogueEditor),false,"Asura Dialog Editor");
+        }
+
+        [OnOpenAssetAttribute(1)]
+        public static bool OnOpenAsetDialogue(int instanceID, int line)
+        {
+            Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+            if (dialogue != null)
+            {
+                ShowEditorWindow();
+                Debug.Log("openWindowDialogue");
+                return true;
+            }
+
+            return false;
         }
     }
-    
 }
