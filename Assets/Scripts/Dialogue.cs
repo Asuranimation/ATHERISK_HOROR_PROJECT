150f653b446f8384aa377a1071b601835cf2be21
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-16 17:52:24 +0700

    Update Dialogue
    rename menuName So ke ScriptableObject

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,14 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DialogueSystem
 {
-    [CreateAssetMenu (fileName = "new Dialogue" , menuName = "SO/Dialogue")]
+    [CreateAssetMenu (fileName = "new Dialogue" , menuName = "ScriptableObject/Dialogue")]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField]  DialogNode[] nodes;
+        [SerializeField] private List<DialogNode> nodes;
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if (nodes.Count == 0)
+            {
+                nodes.Add(new DialogNode()); 
+            }
+        }
+#endif
+        public IEnumerable<DialogNode> GetAllNodes()
+        {
+            return nodes;
+        }
     }
+    
     
 }
 
