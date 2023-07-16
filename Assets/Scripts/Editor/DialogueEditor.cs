586db931e59c74bad7be5ee093cfe58262f041e7
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-16 17:56:45 +0700

    Update DialogueEditor
    Saya mengganti akses membuka dialogue Editor, dari Window ke Tools menu di unity
    
    selanjutnya saya menghilangkan Attribute  dan parameter yang tidak perlu.
    
    saya Update Method OnGui, tujuannya saya ingin membuat kolom text di OnGui dapat di Custom, dan menyimpan perubahan custom text di method setDirty , supaya saya bisa mengimplementasikan Undo kedepannya

--- Assets/Scripts/Editor/DialogueEditor.cs
+++ Assets/Scripts/Editor/DialogueEditor.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -8,15 +10,15 @@ namespace DialogueSystem.Editor
 {
     public class DialogueEditor : EditorWindow
     {
-        [MenuItem("Window/DialogueEditor")]
+        [MenuItem("Tools/DialogueEditor")]
         public static void ShowEditorWindow()
         {
             Debug.Log("anda mengaktifkan dialogueEditor");
             GetWindow(typeof(DialogueEditor),false,"Asura Dialog Editor");
         }
 
-        [OnOpenAssetAttribute(1)]
-        public static bool OnOpenAsetDialogue(int instanceID, int line)
+        [OnOpenAsset(1)]
+        public static bool OnOpenAsetDialogue(int instanceID)
         {
             Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
             if (dialogue != null)
@@ -25,8 +27,45 @@ namespace DialogueSystem.Editor
                 Debug.Log("openWindowDialogue");
                 return true;
             }
-
             return false;
+        }
+
+        private Dialogue selectedDialogue = null;
+
+        private void OnGUI()
+        {
+            if (selectedDialogue != null)
+            {
+                foreach (DialogNode node in selectedDialogue.GetAllNodes())
+                {
+                    /* jadikan new dialog ini ref dari EditorGUILayout.TextField(node.text); berarti nilai string mereka sama
+                     disini jika ada perubahan text maka jelas newDialogue tidak sama dengan node.text , jadi yg ada perubahan 
+                     string aja yg di simpan dalam setDirty, kalo ga gini ya semua disimpan dalam set dirty meski itu perubahan
+                     yg tidak diperlukan, beginilah cara atasinya */
+                    string newDialogue = EditorGUILayout.TextField(node.text);
+                    if (newDialogue != node.text)
+                    {
+                        node.text = newDialogue;
+                        EditorUtility.SetDirty(selectedDialogue);
+                    }
+                    
+
+                }
+            }
+            else
+                EditorGUILayout.LabelField("Dialogue not found");
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+        void OnSelectionChanged()
+        {
+            Dialogue newDialogue = Selection.activeObject as Dialogue;
+            selectedDialogue = newDialogue;
+            Repaint();
         }
     }
 }
