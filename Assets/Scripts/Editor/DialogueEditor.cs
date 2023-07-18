cf336d35c39676df6def22f30d5d187741968655
Sakti Harahap <saktiharahap2606@gmail.com>
2023-07-18 12:36:51 +0700

    Update Dialogue Editor
    di method OnGui, mulai terlihat banyak baris code yang membuatnya kurang rapih, karena itu saya jadikan saja sebagai function OnGuiNode,
    
    lalu saya membuat new method ProcessEvents , yang tujuannya untuk membuat posisi node bisa berubah melalui posisi mouse,
    
    terlihat ada if statement yang berisi kondisi event mouse,
    dragging adalah boolean yang memastikan apakah player sedang dragging node atau tidak, jika mouse di klik tahan dan sedang menggerakkan posisi maka dragging is true , dan ketika true dia akan merekam undo , serta merubah posisi sesuai mouse posisi,
    
    jika mouse up, maka dragging false dan tidak menjalankan undo dan dan tidak menjalankan perubahan posisi

--- Assets/Scripts/Editor/DialogueEditor.cs
+++ Assets/Scripts/Editor/DialogueEditor.cs
@@ -31,36 +31,69 @@ namespace DialogueSystem.Editor
         }
 
         private Dialogue selectedDialogue = null;
-
+        private GUIStyle nodeSytle;
+        private bool dragging = false;
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+            nodeSytle = new GUIStyle();
+            string path = "Assets/Sprites/TexDialogue.png"; // Ubah sesuai dengan jalur relatif yang benars
+            Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+            nodeSytle.normal.background = texture;
+            nodeSytle.padding = new RectOffset(40, 40, 20, 20);
+            nodeSytle.border = new RectOffset(20, 20, -20, 30);
+        }
         private void OnGUI()
         {
             if (selectedDialogue != null)
             {
+                ProcessEvents();
                 foreach (DialogNode node in selectedDialogue.GetAllNodes())
                 {
-                    EditorGUILayout.LabelField("Node");
-                    EditorGUI.BeginChangeCheck();
-                    string newUniqueId = EditorGUILayout.TextField(node.uniqueID);
-                    string newDialogue = EditorGUILayout.TextField(node.text);
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        Undo.RecordObject(selectedDialogue,"Update Dialogue Text");
-                        node.uniqueID = newUniqueId;
-                        node.text = newDialogue;
-                    }
-
+                    OnGuiNode(node);
                 }
             }
             else
+            {
                 EditorGUILayout.LabelField("Dialogue not found");
+            }
         }
 
-        private void OnEnable()
+        private void ProcessEvents()
         {
-            Selection.selectionChanged += OnSelectionChanged;
+            if (Event.current.type == EventType.MouseDown && !dragging)
+            {
+                dragging = true;
+            }
+            else if (Event.current.type == EventType.MouseDrag && dragging)
+            {
+                Undo.RecordObject(selectedDialogue, "Move Dialogue Node");
+                selectedDialogue.GetRootNode().positionNode.position = Event.current.mousePosition;
+                GUI.changed = true;
+            }
+            else if (Event.current.type == EventType.MouseUp && dragging)
+            {
+                dragging = false;
+            }
+        }
+        
+        private void OnGuiNode(DialogNode node)
+        {
+            GUILayout.BeginArea(node.positionNode, nodeSytle);
+            EditorGUILayout.LabelField("Node", new GUIStyle(EditorStyles.boldLabel) { normal = new GUIStyleState() { textColor = Color.black } });
+            EditorGUI.BeginChangeCheck();
+            string newUniqueId = EditorGUILayout.TextField(node.uniqueID);
+            string newDialogue = EditorGUILayout.TextField(node.text);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+                node.uniqueID = newUniqueId;
+                node.text = newDialogue;
+            }
+            GUILayout.EndArea();
         }
 
-        void OnSelectionChanged()
+        private void OnSelectionChanged()
         {
             Dialogue newDialogue = Selection.activeObject as Dialogue;
             selectedDialogue = newDialogue;
